afd532db509e3c71eeeb01496d2c226c6c52767e
Artemis Git Integration <artemis@turintech.ai>
2026-07-01 05:53:31 +0000

    Batch update (1 files)

--- sorter.py
+++ sorter.py
@@ -1,7 +1,3 @@
 def sort_numbers(numbers):
-      n = len(numbers)
-      for i in range(n):
-          for j in range(0, n - i - 1):
-              if numbers[j] > numbers[j + 1]:
-                  numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
-      return numbers
+    numbers.sort()
+    return numbers
